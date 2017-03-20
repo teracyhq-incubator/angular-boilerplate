f0468dfb656c2ab013cb42c4c2139babef616170
hoatle <hoatle@teracy.com>
2017-03-20 11:21:17 +0700

    @ #31 | should use domain aliases for dev and prod modes instead of ports
    Because ports fixed "4200:4200" or "8080:80" are easily conflicted with other services.
    
    We should use nginx-proxy provided by teracy-dev to use domain aliases instead of fixed ports.

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ envsubst '${PROJECT_NAME}' <.angular-cli.tpl.json >.angular-cli.json
 
 # generate package.json
 rm -rf package.json
-envsubst '${PROJECT_NAME} ${PROJECT_VERSION}' <package.tpl.json >package.json
+envsubst '${PROJECT_NAME} ${PROJECT_VERSION} ${DOMAIN_DEV}' <package.tpl.json >package.json
 
 # generate docker-compose.yml
 rm -rf docker-compose.yml
-envsubst '${DOCKER_IMAGE_DEV}' <docker-compose.tpl.yml >docker-compose.yml
+envsubst '${DOCKER_IMAGE_DEV} ${DOMAIN_DEV}' <docker-compose.tpl.yml >docker-compose.yml
 
 # generate docker-compose.prod.yml
 rm -rf docker-compose.prod.yml
-envsubst '${DOCKER_IMAGE_PROD}' <docker-compose.prod.tpl.yml >docker-compose.prod.yml
+envsubst '${DOCKER_IMAGE_PROD} ${DOMAIN_PROD}' <docker-compose.prod.tpl.yml >docker-compose.prod.yml
